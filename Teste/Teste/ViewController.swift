cdf40ecb1bf4d107a47b11f1210a1cc2a5d86b40
Daiane Goncalves <daigoncalves1409@gmail.com>
2023-01-20 13:34:53 -0300

    ajuste exercicio para reuso da função usando idade como parametro

---
+++ Teste/Teste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teste
+//
+//  Created by Daiane Goncalves on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
